4de183fa84be1cdc9e28bbcff28670753a040ecc
Thomas McNiven <hello@vevix.net>
2017-01-22 02:15:38 +1000

    Don't import jQuery/DOM dependent addons when in Fastboot

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,7 +3,6 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    storeConfigInMeta: false,
     babel: {
       comments: false,
       includePolyfill: true
@@ -53,16 +52,19 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  app.import('bower_components/tether/dist/js/tether.min.js');
-  app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
-  app.import('bower_components/nouislider/distribute/nouislider.js');
-  app.import('bower_components/nouislider/distribute/nouislider.min.css');
+  if (!process.env.EMBER_CLI_FASTBOOT) {
+    app.import('bower_components/tether/dist/js/tether.min.js');
+    app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
+    app.import('bower_components/nouislider/distribute/nouislider.js');
+    app.import('bower_components/nouislider/distribute/nouislider.min.css');
+    app.import('bower_components/flickity/dist/flickity.min.css');
+    app.import('bower_components/flickity/dist/flickity.pkgd.min.js');
+    app.import('bower_components/clipboard/dist/clipboard.min.js');
+    app.import('bower_components/autosize/dist/autosize.min.js');
+    app.import('bower_components/jquery-hoverintent/jquery.hoverIntent.js');
+    app.import('bower_components/getstream/dist/js_min/getstream.js');
+  }
   app.import('bower_components/humanize-duration/humanize-duration.js');
-  app.import('bower_components/flickity/dist/flickity.min.css');
-  app.import('bower_components/clipboard/dist/clipboard.min.js');
-  app.import('bower_components/autosize/dist/autosize.min.js');
-  app.import('bower_components/jquery-hoverintent/jquery.hoverIntent.js');
-  app.import('bower_components/getstream/dist/js_min/getstream.js');
 
   return app.toTree();
 };
