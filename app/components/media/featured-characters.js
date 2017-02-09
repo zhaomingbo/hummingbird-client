cc187ee29e501fb80f6de89e69673df726bbd7a3
Thomas McNiven <hello@vevix.net>
2017-02-09 23:09:58 +1000

    Switch featuted-characters component to new API

--- app/components/media/featured-characters.js
+++ app/components/media/featured-characters.js
@@ -2,37 +2,24 @@ import Component from 'ember-component';
 import get from 'ember-metal/get';
 import set from 'ember-metal/set';
 import service from 'ember-service/inject';
-import { capitalize } from 'ember-string';
-import { isPresent } from 'ember-utils';
 import { task } from 'ember-concurrency';
 
 export default Component.extend({
   tagName: 'section',
   classNames: ['media--main-characters'],
-  ajax: service(),
   store: service(),
 
   getCharacters: task(function* (mediaType, mediaId) {
-    let language;
-    if (mediaType === 'anime') {
-      const languages = yield get(this, 'ajax').request(`/anime/${mediaId}/_languages`);
-      if (isPresent(languages)) {
-        language = languages.find(item => item === 'Japanese') || get(languages, 'firstObject');
-      }
-    }
-    return yield get(this, 'store').query('casting', {
-      filter: Object.assign({
-        media_id: mediaId,
-        media_type: capitalize(mediaType),
-        is_character: true
-      }, { language }),
+    return yield get(this, 'store').query(`${mediaType}-character`, {
+      filter: { [`${mediaType}_id`]: mediaId },
       include: 'character',
-      sort: '-featured',
+      sort: 'role',
       page: { limit: 4 }
     });
   }).restartable(),
 
   didReceiveAttrs() {
+    this._super(...arguments);
     const type = get(this, 'media.modelType');
     const id = get(this, 'media.id');
     const taskInstance = get(this, 'getCharacters').perform(type, id);
---
+++ app/models/anime-character.js
@@ -0,0 +1,11 @@
+import Base from 'client/models/-base';
+import attr from 'ember-data/attr';
+import { belongsTo, hasMany } from 'ember-data/relationships';
+
+export default Base.extend({
+  role: attr('string'),
+
+  anime: belongsTo('anime'),
+  character: belongsTo('character'),
+  castings: hasMany('casting')
+});
